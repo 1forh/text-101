598465e96fd88548f810095c99169d1f9491388a
1forh <1forhlol@gmail.com>
2020-05-19 17:12:34 -0400

    updates

---
+++ Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] private Text storyText;
+    [SerializeField] private State startingState;
+
+    private State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        storyText.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageState();
+    }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+
+        storyText.text = state.GetStateStory();
+    }
+}
